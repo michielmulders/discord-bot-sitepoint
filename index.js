08259b8ca39e3754c647c846855a675476dc2c51
michielmulders <michielmulders1@gmail.com>
2019-10-15 16:12:28 +0200

    Add simple code example

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-/* Discord Bot SitePoint */
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+
+    } else if (msg.content.startsWith('!kick')) {
+        if (msg.mentions.users.size) {
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+        } else {
+            msg.reply('Please tag a valid user!');
+        }
+    }
+});
